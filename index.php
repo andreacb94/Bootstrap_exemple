0750e534f10db47c582cf32eb661863f60782c9a
Andrea <andreacb94>
2017-09-13 17:08:52 +0200

    Inizial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrea
+ * Date: 10/09/17
+ * Time: 21:07
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+
+    <title>Bootstrap_Exemple</title>
+    <meta charset="utf-8">
+
+    <!-- Gestione del responsive (vedi http://www.html.it/pag/33420/il-meta-tag-viewport/) -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+</head>
+
+    <body>
+
+        <div class="container-fluid">
+
+            <h1>Benvenuto! Questo è il sito di test di Bootstrap3!</h1>
+
+        </div>
+
+        <div class="container-fluid">
+
+            <h2>Qui puoi trovare gli esempi su bootstrap</h2>
+
+            <h3>Accedi alla pagina desiderata</h3>
+            <a href="test_bootstrap_pages/test_griglie.html" type="button" class="btn btn-default">Test griglie</a>
+            <a href="test_bootstrap_pages/test_button.html" type="button" class="btn btn-primary">Test button</a>
+            <a href="test_bootstrap_pages/test_form.html" type="button" class="btn btn-info">Test form</a>
+
+        </div>
+
+        <!-- Script da caricare, meglio qui così la grafica appare prima all'utente -->
+        <!-- jQuery e plugin JavaScript  -->
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+    </body>
+
+</html>
